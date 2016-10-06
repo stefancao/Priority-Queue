968f3b7ba2e9c19f34dc5f588225ff9a78798312
Stefan Cao <stefancao@Stefans-iMac.local>
2016-10-05 23:31:55 -0700

    cleaned up code

--- PriorityQueue.java
+++ PriorityQueue.java
@@ -2,22 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Course: EECS 114 Fall 2015
- *
  * First Name: Stefan
  * Last Name: Cao
- * Lab Section: 1A
- * email address: stefanc1@uci.edu
  *
- *
- * Assignment: assignment4
  * Filename : PriorityQueue.java
- *
- * I hereby certify that the contents of this file represent
- * my own original individual work. Nowhere herein is there 
- * code from any outside resources such as another individual,
- * a website, or publishings unless specifically designated as
- * permissible by the instructor or TA.
  */ 
 
 public class PriorityQueue {
--- PriorityQueueMain.java
+++ PriorityQueueMain.java
@@ -1,20 +1,8 @@
 /**
- * Course: EECS 114 Fall 2015
- *
  * First Name: Stefan
  * Last Name: Cao
- * Lab Section: 1A
- * email address: stefanc1@uci.edu
  *
- *
- * Assignment: assignment4
  * Filename : PriorityQueueMain.java
- *
- * I hereby certify that the contents of this file represent
- * my own original individual work. Nowhere herein is there 
- * code from any outside resources such as another individual,
- * a website, or publishings unless specifically designated as
- * permissible by the instructor or TA.
  */ 
 
 public class PriorityQueueMain {
